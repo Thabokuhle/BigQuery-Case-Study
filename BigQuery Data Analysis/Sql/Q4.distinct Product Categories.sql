8c9f0ec04b4218fc55c02d690f80045cd111b703
Thabokuhle <thabokuhlekhoza@gmail.com>
2025-12-10 01:12:18 +0200

    Create Q4.distinct Product Categories.sql

---
+++ BigQuery Data Analysis/Sql/Q4.distinct Product Categories.sql
@@ -0,0 +1,6 @@
+--4. DISTINCT
+----Question4. Display all distinct Product Categories in the dataset.
+SELECT DISTINCT `Product Category`
+FROM `plated-hangout-480700-v9.Retail_data.Retail_sales`;
+
+  
