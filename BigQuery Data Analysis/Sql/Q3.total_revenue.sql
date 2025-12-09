5ac10e1323f9dbbdc8209173cb537d620cb4f556
Thabokuhle <thabokuhlekhoza@gmail.com>
2025-12-10 00:37:14 +0200

    Create Q3.total_revenue.sql

---
+++ BigQuery Data Analysis/Sql/Q3.total_revenue.sql
@@ -0,0 +1,5 @@
+--3. Aggregate Functions
+----Question3. Calculate the total revenue (sum of Total Amount).
+SELECT 
+    SUM(`Total Amount`) AS Total_Revenue
+FROM `plated-hangout-480700-v9.Retail_data.Retail_sales`;
