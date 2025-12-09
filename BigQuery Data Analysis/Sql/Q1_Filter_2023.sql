8cd71043639dadeee13a7e714490bac9ec9e15a8
Thabokuhle <thabokuhlekhoza@gmail.com>
2025-12-10 00:32:49 +0200

    Update Q1_Filter_2023.sql

--- BigQuery Data Analysis/Sql/Q1_Filter_2023.sql
+++ BigQuery Data Analysis/Sql/Q1_Filter_2023.sql
@@ -1,4 +1,5 @@
-Question1_Filter_2023.sql
+--1. WHERE Clause
+----Question1. Filter all transactions that occurred in the year 2023.
 SELECT *
 FROM `plated-hangout-480700-v9.Retail_data.Retail_sales`
 WHERE EXTRACT(YEAR FROM Date) = 2023;
